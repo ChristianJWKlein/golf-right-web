e2dba76752d4f0396efc69988e3df4347671154b
Christian JW Klein <cklein010715@gmail.com>
2022-03-09 16:15:23 -0500

    Hero added  checkpoint

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import { Typography, Grid, Button } from '@mui/material';
 
 export default function Hero() {
   return (
     <>
-      <h1>Hero</h1>
+      <section className='hero'>
+        <section className='hero-content'>
+          <Typography
+            component='h1'
+            variant='h2'
+            align='center'
+            color='textPrimary'
+            gutterBottom
+          >
+            Golf Right
+          </Typography>
+          <Typography
+            variant='h6'
+            align='center'
+            color='textSecondary'
+            paragraph
+          >
+            Stuck up Golf Courses are a thing of the past. <br />
+            Find the right course for you.
+          </Typography>
+          <section>
+            <Grid container spacing={2} justify='center'>
+              <Grid item>
+                <Button variant='contained' color='primary'>
+                  Find your Course
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button variant='outlined' color='primary'>
+                  Rate a Course
+                </Button>
+              </Grid>
+            </Grid>
+          </section>
+        </section>
+      </section>
     </>
   );
 }
--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -2,9 +2,21 @@ import { AppBar, Toolbar, Typography } from '@mui/material';
 import React from 'react';
 
 export default function MenuBar() {
+  const displayDesktop = () => {
+    return <Toolbar>{golfRightLogo}</Toolbar>;
+  };
+
+  const golfRightLogo = (
+    <Typography variant='h6' component='h1'>
+      Golf Right
+    </Typography>
+  );
+
   return (
     <>
-      <h1>MenuBar</h1>
+      <header>
+        <AppBar className='header'>{displayDesktop()}</AppBar>
+      </header>
     </>
   );
 }
